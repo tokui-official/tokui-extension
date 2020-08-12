73f7a0b99996263ca2909ce411c346749d8bd6bf
Truong <nguyentientruong95@gmail.com>
2020-08-12 23:21:17 +0900

    git init and project base

---
+++ src/new_tab/router/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Welcome to tokui system</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
